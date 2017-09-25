480758728b3438593230cad244e532495c24676f
Sahil Thakar <dharaanilkumar43@gmail.com>
2017-09-24 23:19:38 -0700

    Change Voice like tomcat

---
+++ app/src/main/java/com/example/abc/voice/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.abc.voice;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity  {
+
+    Integer[] freqset = {11025, 16000, 22050, 44100};
+    private ArrayAdapter<Integer> adapter;
+
+    Spinner spFrequency;
+    Button startRec, stopRec, playBack;
+
+    Boolean recording;    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startRec = (Button)findViewById(R.id.startrec);
+        stopRec = (Button)findViewById(R.id.stoprec);
+        playBack = (Button)findViewById(R.id.playback);
+
+        startRec.setOnClickListener(startRecOnClickListener);
+        stopRec.setOnClickListener(stopRecOnClickListener);
+        playBack.setOnClickListener(playBackOnClickListener);
+
+        spFrequency = (Spinner)findViewById(R.id.frequency);
+        adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, freqset);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spFrequency.setAdapter(adapter);
+
+    }
+
+    View.OnClickListener startRecOnClickListener
+            = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View arg0) {
+
+            Thread recordThread = new Thread(new Runnable(){
+
+                @Override
+                public void run() {
+                    recording = true;
+                    startRecord();
+                }
+
+            });
+
+            recordThread.start();
+
+        }};
+
+    View.OnClickListener stopRecOnClickListener
+            = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View arg0) {
+            recording = false;
+        }};
+
+    View.OnClickListener playBackOnClickListener
+            = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            playRecord();
+        }
+
+    };
+
+    private void startRecord(){
+
+        File file = new File(Environment.getExternalStorageDirectory(), "test.pcm");
+
+        int sampleFreq = (Integer)spFrequency.getSelectedItem();
+
+        try {
+            file.createNewFile();
+
+            OutputStream outputStream = new FileOutputStream(file);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+            DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream);
+
+            int minBufferSize = AudioRecord.getMinBufferSize(sampleFreq,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT);
+
+            short[] audioData = new short[minBufferSize];
+
+            AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    sampleFreq,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    minBufferSize);
+
+            audioRecord.startRecording();
+
+            while(recording){
+                int numberOfShort = audioRecord.read(audioData, 0, minBufferSize);
+                for(int i = 0; i < numberOfShort; i++){
+                    dataOutputStream.writeShort(audioData[i]);
+                }
+            }
+
+            audioRecord.stop();
+            dataOutputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    void playRecord(){
+
+        File file = new File(Environment.getExternalStorageDirectory(), "test.pcm");
+
+        int shortSizeInBytes = Short.SIZE/Byte.SIZE;
+
+        int bufferSizeInBytes = (int)(file.length()/shortSizeInBytes);
+        short[] audioData = new short[bufferSizeInBytes];
+
+        try {
+            InputStream inputStream = new FileInputStream(file);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+
+            int i = 0;
+            while(dataInputStream.available() > 0){
+                audioData[i] = dataInputStream.readShort();
+                i++;
+            }
+
+            dataInputStream.close();
+
+            int sampleFreq = (Integer)spFrequency.getSelectedItem();
+
+            AudioTrack audioTrack = new AudioTrack(
+                    AudioManager.STREAM_MUSIC,
+                    sampleFreq,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSizeInBytes,
+                    AudioTrack.MODE_STREAM);
+
+            audioTrack.play();
+            audioTrack.write(audioData, 0, bufferSizeInBytes);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
